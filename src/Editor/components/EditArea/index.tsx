6984a06e39eff4eaf2e1c00ddc750efafda087aa
hr <heru@qq.com>
2024-12-12 18:00:49 +0800

    画布区 hover 展示高亮框

--- src/Editor/components/EditArea/index.tsx
+++ src/Editor/components/EditArea/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useComponentsStore } from '../../stores/components'
 import { useComponentConfigStore } from '../../stores/component-config'
 import type { Component } from '../../stores/components'
+import type { MouseEventHandler } from 'react'
+import HoverMask from '../HoverMask'
 
 export function EditArea() {
   const { components } = useComponentsStore()
@@ -28,5 +30,50 @@ export function EditArea() {
     })
   }
 
-  return <div className="h-[100%]">{renderComponents(components)}</div>
+  const [hoverComponentId, setHoverComponentId] = useState<number>()
+
+  const handleMouseOver: MouseEventHandler = (e) => {
+    /**
+     * 在React的事件对象中，nativeEvent 是一个指向原生DOM事件的引用。这个原生事件对象包含了浏览器提供的所有事件属性和方法
+     * composedPath 是一个返回事件触发链（也称为“事件路径”）的方法。它返回一个数组，包含了从事件的目标节点开始，一直到文档的根节点（document 或 shadow root）的所有节点
+     */
+    // const path = e.nativeEvent.composedPath()
+    // for (let i = 0; i < path.length; i++) {
+    //   const ele = path[i] as HTMLElement
+    //   const componentId = ele.dataset.componentId
+    //   if (componentId) {
+    //     setHoverComponentId(+componentId)
+    //     return
+    //   }
+    // }
+
+    const node = e.target as HTMLElement
+    // Element.closest() 方法用来获取：匹配特定选择器且离当前元素最近的祖先元素（也可以是当前元素本身）。如果匹配不到，则返回 null。
+    const target = node.closest('[data-component-id]') as HTMLElement
+    if (target) {
+      // const componentId = target.getAttribute('data-component-id')
+      const componentId = target.dataset.componentId
+      if (componentId) {
+        setHoverComponentId(+componentId)
+      }
+    }
+  }
+
+  return (
+    <div
+      className="edit-area h-[100%]"
+      onMouseOver={handleMouseOver}
+      onMouseLeave={() => setHoverComponentId(undefined)}
+    >
+      {renderComponents(components)}
+      {hoverComponentId && (
+        <HoverMask
+          portalWrapperClassName="portal-wrapper"
+          containerClassName="edit-area"
+          componentId={hoverComponentId}
+        />
+      )}
+      <div className="portal-wrapper"></div>
+    </div>
+  )
 }
---
+++ src/Editor/components/HoverMask/index.tsx
@@ -0,0 +1,117 @@
+import { useEffect, useMemo, useState } from 'react'
+import { createPortal } from 'react-dom'
+import { getComponentById, useComponentsStore } from '../../stores/components'
+
+interface HoverMaskProps {
+  portalWrapperClassName: string
+  containerClassName: string
+  componentId: number
+}
+
+// containerClassName 是画布区的根元素的 className, componentId 是 hover 的组件 id
+function HoverMask({
+  portalWrapperClassName,
+  containerClassName,
+  componentId,
+}: HoverMaskProps) {
+  const [position, setPosition] = useState({
+    left: 0,
+    top: 0,
+    width: 0,
+    height: 0,
+    labelTop: 0,
+    labelLeft: 0,
+  })
+
+  const { components } = useComponentsStore()
+
+  useEffect(() => {
+    updatePosition()
+  }, [componentId])
+
+  function updatePosition() {
+    if (!componentId) return
+
+    const container = document.querySelector(`.${containerClassName}`)
+    if (!container) return
+
+    const node = document.querySelector(`[data-component-id="${componentId}"]`)
+    if (!node) return
+
+    const { top, left, width, height } = node.getBoundingClientRect()
+    const { top: containerTop, left: containerLeft } =
+      container.getBoundingClientRect()
+
+    let labelTop = top - containerTop + container.scrollTop
+    let labelLeft = left - containerLeft + width
+
+    if (labelTop <= 0) {
+      labelTop -= -20
+    }
+
+    setPosition({
+      top: top - containerTop + container.scrollTop,
+      left: left - containerLeft + container.scrollLeft,
+      width,
+      height,
+      labelTop,
+      labelLeft,
+    })
+  }
+
+  const el = useMemo(() => {
+    return document.querySelector(`.${portalWrapperClassName}`)!
+  }, [])
+
+  const curComponent = useMemo(() => {
+    return getComponentById(componentId, components)
+  }, [componentId])
+
+  return createPortal(
+    <>
+      <div
+        style={{
+          position: 'absolute',
+          left: position.left,
+          top: position.top,
+          backgroundColor: 'rgba(0, 0, 255, 0.1)',
+          border: '1px dashed blue',
+          // 除了指示该元素不是鼠标事件的目标之外，值none表示鼠标事件“穿透”该元素并且指定该元素“下面”的任何东西。
+          pointerEvents: 'none',
+          width: position.width,
+          height: position.height,
+          zIndex: 12,
+          borderRadius: 4,
+          boxSizing: 'border-box',
+        }}
+      />
+      <div
+        style={{
+          position: 'absolute',
+          left: position.labelLeft,
+          top: position.labelTop,
+          fontSize: '14px',
+          zIndex: 13,
+          display: !position.width || position.width < 10 ? 'none' : 'inline',
+          transform: 'translate(-100%, -100%)',
+        }}
+      >
+        <div
+          style={{
+            padding: '0 8px',
+            backgroundColor: 'blue',
+            borderRadius: 4,
+            color: '#fff',
+            cursor: 'pointer',
+            whiteSpace: 'nowrap',
+          }}
+        >
+          {curComponent?.name}
+        </div>
+      </div>
+    </>,
+    el
+  )
+}
+
+export default HoverMask
