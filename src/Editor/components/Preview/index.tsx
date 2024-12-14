a26473ab9a578a1fccaa75895aca79fdd9c632b2
hr <heru@qq.com>
2024-12-14 15:48:19 +0800

    事件绑定

--- src/Editor/components/Preview/index.tsx
+++ src/Editor/components/Preview/index.tsx
@@ -1,10 +1,38 @@
 import React from 'react'
-import { Component, useComponentsStore } from '../../stores/components'
+import { message } from 'antd'
+import { useComponentsStore } from '../../stores/components'
 import { useComponentConfigStore } from '../../stores/component-config'
+import type { Component } from '../../stores/components'
 
 export function Preview() {
   const { components } = useComponentsStore()
   const { componentConfig } = useComponentConfigStore()
+
+  function handleEvent(component: Component) {
+    const props: Record<string, any> = {}
+
+    componentConfig[component.name].events?.forEach((event) => {
+      const eventConfig = component.props[event.name]
+
+      if (eventConfig) {
+        const { type } = eventConfig
+
+        props[event.name] = () => {
+          if (type === 'goToLink' && eventConfig.url) {
+            window.location.href = eventConfig.url
+          } else if (type === 'showMessage' && eventConfig.config) {
+            if (eventConfig.config.type === 'success') {
+              message.success(eventConfig.config.text)
+            } else if (eventConfig.config.type === 'error') {
+              message.error(eventConfig.config.text)
+            }
+          }
+        }
+      }
+    })
+
+    return props
+  }
 
   function renderComponents(components: Component[]): React.ReactNode {
     return components.map((component) => {
@@ -23,6 +51,7 @@ export function Preview() {
           styles: component.styles,
           ...config.defaultProps,
           ...component.props,
+          ...handleEvent(component),
         },
         renderComponents(component.children || [])
       )
